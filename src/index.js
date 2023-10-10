aa472a319770a1d962b253c129cb80569ae6aa3e
OclaZ <aslikhhamza2001@gmail.com>
2023-10-11 00:45:06 +0100

    commit2

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
+import { useState } from "react";
 
+function Test() {
+  const [movieRating, setMovieRaing] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRaing} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={7} color="red" size={12} defaulRating={3} />
+    <Test />
   </React.StrictMode>
 );
