9354a6640932022a1649aa311557563d0f592239
OclaZ <aslikhhamza2001@gmail.com>
2023-10-10 23:52:10 +0100

    commit2

--- src/index.js
+++ src/index.js
@@ -8,6 +8,5 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
   </React.StrictMode>
 );
